3140d0bedce840021fd71bb5616daa6a10201e79
Alejandro <hramirez03@alumnos.uaq.mx>
2024-04-19 22:51:13 -0600

    modified:   back/src/routes/transportistas.js 	modified:   front/js/agregar.js

--- back/src/routes/transportistas.js
+++ back/src/routes/transportistas.js
@@ -1,28 +1,41 @@
 // Conexion a la base de datos
 const express = require('express')
 const empleados = express.Router()
-
+const client = require('./connection');
 
 async function main(){
     try {
+        // Conectarse al MongoDB cluster
+        await client.connect();
 
-        empleados.post('/agregar', async (req, res, next) => {
-            const { nombre_empleado, apellidos_empleado, telefono_empleado, correo_empleado, direccion_empleado } = req.body
-        
-            if (nombre_empleado && apellidos_empleado && telefono_empleado && correo_empleado && direccion_empleado) {
-                let query = "INSERT INTO empleados(nombre_empleado, apellidos_empleado, telefono_empleado, correo_empleado, direccion_empleado)"
-                query += ` VALUES('${nombre_empleado}', '${apellidos_empleado}', ${telefono_empleado}, '${correo_empleado}', '${direccion_empleado}')`
-        
-                const rows = await db.query(query)
-        
-                if (rows.affectedRows == 1) {
-                    return res.status(200).json({code: 201, message: "Empleado agregado correctamente"})
+        // Acceder a la base de datos y coleccion
+        const database = client.db("Hackathoon");
+        const collection = database.collection("Transportistas");
+
+        var nombre = document.getElementById('#').value
+        var telefono = document.getElementById('#').value
+        var correo = document.getElementById('#').value
+        var id_empresa = "#" //esta lo tiene que obtener de manera automatica dependiendo de quien sea el que lo registro
+    
+        if (nombre && telefono && correo && id_empresa) {
+            if (!nombre.length == 0 && !telefono.length == 0 && !correo.length == 0 && !id_empresa == 0) {
+                var numero = parseInt(telefono)
+                if((validarNumero(numero)) && (validarEmail(correo))){                
+                    const newDoc = {
+                        nombre: nombre,
+                        telefono: numero,
+                        correo: correo,
+                        id_empresa: id_empresa
+                    };
+                    // Insert the document into the collection
+                    const result = collection.insertOne(newDoc);
+                    console.log(`${result.insertedCount} Documento insertado con ID: ${result.insertedId}`);
                 }
-                return res.status(500).json({code: 500, message: "Ocurrió un error"})
             }
-            return res.status(500).json({code: 500, message: "Campos incompletos"})
-        })
+        
+        }
 
+        
     } catch (e) {
         console.error(e);
     } finally {
@@ -30,6 +43,31 @@ async function main(){
     }
 
 }
+
+function validarNumero(valor) {
+    var numero = parseInt(valor)
+
+    if ((Number.isInteger(numero))) {
+        return true
+    } 
+    else {
+        alert("Ingresa un teléfono válido");
+    }
+}
+
+function validarEmail(valor) {
+    var correo = valor.toString()
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+
+    if (emailRegex.test(correo)){
+        return true
+    } 
+    else {
+        alert("Ingresa un correo válido");
+    }
+}
+
+
 
 main().catch(console.error);
 
--- front/js/agregar.js
+++ front/js/agregar.js
@@ -1,5 +1,5 @@
 window.onload = init
-const client = require('hackathon\back\src\routes\connection.js');
+
 function init(){
 
 }
@@ -14,36 +14,6 @@ function agregar_t(){
         var numero = parseInt(telefono)
         if((validarNumero(numero)) && (validarEmail(correo))){
             
-            // Conectarse al MongoDB cluster
-            client.connect();
-
-            // Acceder a la base de datos y coleccion
-            const database = client.db("Hackathoon");
-            const collection = database.collection("Transportistas");
-
-            // const { nombre, direccion, correo } = req.body;
-            const nombre = 'Federico Peluche';
-            const direccion = 'Av. Siempre Viva #2';
-            const correo =  'VendoPozole@gmail.com';
-            
-            // Crear documento
-            const document = {
-                nombre: nombre,
-                direccion: direccion,
-                correo: correo
-            };
-            // Insertar el documento
-            const result = await collection.insertOne(document);
-
-        console.log(`${result.insertedCount} 'Transportista agregado exitosamente con ID: ${result.insertedId}`);
-            
-            axios({
-                method: 'post', 
-                url: 'http://localhost:3000/transportistas/agregar_transportista',
-                data:{
-
-                }
-            })
         }
     }
 
